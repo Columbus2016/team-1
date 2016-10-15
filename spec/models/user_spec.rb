ba0f283180cb56c1eccda71288a4e3d5874d1a77
Anthony Super <anthony@noided.media>
2016-10-15 02:04:19 -0400

    TDD yo

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,16 @@ RSpec.describe User, type: :model do
   describe "creation" do
 
   end
+
+  describe "visibility" do
+    let(:visible) { create(:user, invisible: false) }
+    let(:invisible) { create(:user, invisible: true) }
+    it "has a scope for visible users" do
+      expect(User.visible).to contain_exactly(visible)
+    end
+
+    it "has a scope for invisible users" do
+      expect(User.invisible).to contain_exactly(invisible)
+    end
+  end
 end
