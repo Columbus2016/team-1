29a87c04e6a344c38a2ec9900e55151a0039aaf7
Anthony Super <anthony@noided.media>
2016-10-15 06:25:24 -0400

    It's 'i before e EXCEPT after c'. Forgot about that last bit #EpicFail

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,7 @@
 class Friendship < ApplicationRecord
+  belongs_to :sender,
+    class_name: "User"
+
+  belongs_to :receiver,
+    class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015102032) do
+ActiveRecord::Schema.define(version: 20161015102422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20161015102032) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "sender_id",   null: false
-    t.integer  "reciever_id", null: false
+    t.integer  "receiver_id", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
