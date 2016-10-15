dceaa2012c4ecf401db4a498c3d30c6e1e692df7
Anthony Super <anthony@noided.media>
2016-10-15 04:13:40 -0400

    USERS CAN SIGN UP #EpicFail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_paramters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
 
   def configure_permitted_parameters
-    devise_paramter_sanitizer.for(:sign_up)
-      .push(:location_lat, :location_long, :birthdate, :handle)
+    devise_parameter_sanitizer
+      .permit(:sign_up, keys: [:location_lat, :location_long, :birthdate, :handle, :gender])
   end
 end
 
