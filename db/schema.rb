89e05b5ed0ca4cb96a6b21bf453bd813b39b115f
Nikhil Vinay <vinay.2@osu.edu>
2016-10-15 08:38:25 -0400

    Created a mockup of the chat function. Not functional, but gives and idea of what it would look like. Colors still need to be changed up a little bit

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20161015102422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
+  enable_extension "postgis_topology"
 
   create_table "cancers", force: :cascade do |t|
     t.string   "name",       null: false
